7d51c3407f6382243b279ccab5a1417ba22fb15a
tajkir-alam <tajkir.alam@gmail.com>
2023-07-21 20:46:14 +0600

    title bg img added

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,11 +1,20 @@
+import Image from 'next/legacy/image';
 import React from 'react';
 
 const Blog = () => {
-    return (
-        <div>
-          this is blog page  
-        </div>
-    );
+  return (
+    <section className='mt-12 relative w-full h-52 blur-[2.5px]'>
+        <Image
+          src="/titleBanner.png"
+          alt="Title background"
+          // width={100}
+          // height={0}
+          layout='fill'
+          sizes='100'
+          priority={true}
+        />
+    </section>
+  );
 };
 
 export default Blog;
