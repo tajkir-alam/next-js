f244d70416a9512c0c62917f93d2ed5206b06757
tajkir-alam <tajkir.alam@gmail.com>
2023-07-21 21:51:17 +0600

    title banner done

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -3,7 +3,8 @@ import React from 'react';
 
 const Blog = () => {
   return (
-    <section className='mt-12 relative w-full h-52 blur-[2.5px]'>
+    <section className='mt-12 relative'>
+      <div className='relative w-full h-52 blur-[2.5px]'>
         <Image
           src="/titleBanner.png"
           alt="Title background"
@@ -13,6 +14,18 @@ const Blog = () => {
           sizes='100'
           priority={true}
         />
+      </div>
+      <div className='uppercase absolute top-1/4 w-full text-center'>
+          <h1 className='text-3xl md:text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#4ACBFF] from-25% to-[#166BFF] to-70% z-50'>
+            Our Blogs
+          </h1>
+          <h3 className='text-[#166BFF] font-semibold text-md md:text-lg my-2'>
+            Uncover a World of Stories and Knowledge
+          </h3>
+          <p className='text-[#4A4A4A] text-sm md:text-md'>
+            Be the first to receive our latest news, guides, discounts, and offers.
+          </p>
+      </div>
     </section>
   );
 };
