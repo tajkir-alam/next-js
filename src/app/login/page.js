a8235086fba5d8a47bae3cb7f8c4a2a3eb427311
tajkir-alam <tajkir.alam@gmail.com>
2023-07-27 00:53:38 +0600

    login page design and responsive done

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -2,15 +2,22 @@ import React from 'react';
 
 const page = () => {
     const backgroundImageStyle = {
-        backgroundImage: "url('/authenticationBG.jpg')"
+        backgroundImage: "url('/authenticationBG.png')"
     };
     const bgOverlayStyle = {
         background: 'linear-gradient(180deg, rgba(0, 0, 0, 0.00) 0.42%, rgba(0, 0, 0, 0.50) 100%)'
     }
 
     return (
-        <div className='w-full h-[40rem] bg-center bg-no-repeat rounded-3xl relative' style={backgroundImageStyle}>
-            <div className='absolute inset-0 bg-[rgba(0,0,0,0.3)]'></div>
+        <div className='w-full h-screen bg-center bg-no-repeat bg-cover rounded-3xl relative' style={backgroundImageStyle}>
+            <div className="lg:mx-44 lg:py-32 grid grid-cols-3 z-20">
+                <div className='bg-[#319DFF] '>
+<h1>Welcome Back</h1>
+                </div>
+                <div className='col-span-2'>
+dsf
+                </div>
+            </div>
         </div>
     );
 };
--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -1,11 +1,68 @@
+"use client"
 import React from 'react';
+import { useForm } from "react-hook-form"
+import { FaRegEnvelope, FaRegUser } from 'react-icons/fa';
+import { HiOutlineLockClosed } from "react-icons/hi2";
 
-const page = () => {
+const Page = () => {
+    const backgroundImageStyle = {
+        backgroundImage: "url('/authenticationBG.png')"
+    };
+    const bgOverlayStyle = {
+        background: 'linear-gradient(180deg, rgba(0, 0, 0, 0.00) 0.42%, rgba(0, 0, 0, 0.50) 100%)'
+    }
+
+    const { register, handleSubmit } = useForm()
+    const onSubmit = (data) => console.log(data)
+
     return (
-        <div>
-            this is signup page
+        <div className='w-full h-fit bg-center bg-no-repeat bg-cover rounded-3xl relative' style={backgroundImageStyle}>
+            <div className="mx-4 lg:mx-44 py-20 flex flex-col items-center lg:items-stretch lg:flex-row">
+                <div className='bg-[#319DFF] py-4 lg:py-20 px-4 lg:px-0 flex flex-col justify-center items-center text-white text-center w-full lg:w-[40%] space-y-5'>
+                    <h1 className='text-4xl lg:text-5xl'>Welcome Back</h1>
+                    <p className='text-sm'>
+                        To keep connected with us please <br />
+                        login with your personal info
+                    </p>
+                    <button className="btn btn-outline border-2 border-white hover:border-white text-white hover:bg-transparent rounded-3xl px-12">
+                        sign in
+                    </button>
+                </div>
+                <div className='w-full lg:w-[60%] py-12 lg:py-36 backdrop-sepia-0 bg-white/30'>
+                    <div className="text-white">
+                        <div className='text-center'>
+                            <h1 className='text-4xl lg:text-5xl text-[#319DFF]'>Create Account</h1>
+                            <p className='text-sm mt-3'>
+                                Use your email for registration
+                            </p>
+                        </div>
+                        <div className='mt-6'>
+                            <form onSubmit={handleSubmit(onSubmit)} className='w-full px-6 lg:px-28'>
+                                <label htmlFor="name" className='flex gap-1 items-center'>
+                                    <FaRegUser />
+                                    <p>Name</p>
+                                </label>
+                                <input {...register("name")} className='w-full mb-5 bg-transparent border-b-2 outline-none px-1 border-b-white' />
+                                <label htmlFor="email" className='flex gap-1 items-center'>
+                                    <FaRegEnvelope />
+                                    <p>Email</p>
+                                </label>
+                                <input {...register("email")} className='w-full mb-5 bg-transparent border-b-2 outline-none px-1 border-b-white' />
+                                <label htmlFor="password" className='flex gap-1 items-center'>
+                                    <HiOutlineLockClosed className='text-xl' />
+                                    <p>Password</p>
+                                </label>
+                                <input {...register("password")} className='w-full mb-5 bg-transparent border-b-2 outline-none px-1 border-b-white' />
+                                <span className='flex justify-center items-center'>
+                                    <input type="submit" className='btn bg-[#319DFF] rounded-3xl text-white border-0 px-12 hover:bg-[#319DFF]' />
+                                </span>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
 
-export default page;
+export default Page;
